fb5d2c22583e0e4d8907cb35a4379fe4b8fc712d
Zach <hzballard2494@gmail.com>
2021-01-04 20:51:09 -0500

    home carousel edit

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
 $(function(){
+    // carousel functionality
+    $(".carousel").carousel( { interval: 3000 } );
+    $("#carouselButton").on('click',() => {
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause"); 
+        }
+    });
+
     // activating the login modal
     $('#loginButton').on('click',() => {
         $('#loginModal').modal('show');
