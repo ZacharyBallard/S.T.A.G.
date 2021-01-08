1b9bc9e678205a82686f3461a6a70b3dc7835c24
Zach <hzballard2494@gmail.com>
2021-01-08 14:48:52 -0500

    build ver.1

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,4 @@
-$(function(){
-    // carousel functionality
-    $(".carousel").carousel( { interval: 3000 } );
-    $("#carouselButton").on('click',() => {
-        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-            $(".carousel").carousel("pause");
-            $("#carouselButton").children("i").removeClass("fa-pause");
-            $("#carouselButton").children("i").addClass("fa-play");
-        } else {
-            $(".carousel").carousel("cycle");
-            $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause"); 
-        }
-    });
-
-    // activating the login modal
-    $('#loginButton').on('click',() => {
-        $('#loginModal').modal('show');
-    });
-});
-
-
-// tooltips
-
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+});
