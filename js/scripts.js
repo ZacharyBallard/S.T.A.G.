9baba001b338eb110099c3c27dde772085f2a72a
Zach <hzballard2494@gmail.com>
2021-01-08 13:37:38 -0500

    STAG version 1.0

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,9 @@ $(function(){
     });
 });
 
+
+// tooltips
+
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
