7e9bf55d13d84b5e732bab040a95d4b46bcbf191
Zach <hzballard2494@gmail.com>
2021-01-09 12:56:42 -0500

    ver.1 feedback update

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,25 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 });
+
+
+$(function() {
+  // carousel functionality 
+  $(".carousel").carousel( { interval: 3000 } );
+  $("#carouselButton").on('click',() => {
+      if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+          $(".carousel").carousel("pause");
+          $("#carouselButton").children("i").removeClass("fa-pause");
+          $("#carouselButton").children("i").addClass("fa-play");
+      } else {
+          $(".carousel").carousel("cycle");
+          $("#carouselButton").children("i").removeClass("fa-play");
+          $("#carouselButton").children("i").addClass("fa-pause"); 
+      }
+  });
+
+  // activating the login modal
+  $('#loginButton').on('click',() => {
+      $('#loginModal').modal('show');
+  });
+});
